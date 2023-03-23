9080ba67edd02fe25b4485e3803dd1d5df106670
Georgina <faithwangare750@gmail.com>
2023-03-23 10:02:37 +0300

    completed code challenge

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
+// Adding an event listener
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const taskDescription = document.getElementById('new-task-description').value;
+    const taskElement = document.createElement('li');
+    taskElement.textContent = taskDescription;
+    taskList.appendChild(taskElement);
+    document.getElementById('new-task-description').value = '';
+  });
+  });
